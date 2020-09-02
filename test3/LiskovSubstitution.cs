bfde052bffd9d72902dd7bd42cd296dfe5e1afed
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-09-02 13:48:34 +0200

    Liskov

--- test3/LiskovSubstitution.cs
+++ test3/LiskovSubstitution.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//L i SOLID
 
-namespace test3
+using test3;
+
+namespace Solid.Liskov
 {
-    class LiskovSubstitution
-    {
-    }
-}
+	class Lsp : IPrinciple
+	{
+		public string Principle()
+		{
+			return "Liskov Substitution";
+		}
+		class Enquiry : Customer
+		{
+			public override int Discount(int sales)
+			{
+				return this.BaseDiscount - (sales * 5);
+			}
+			// This method doesnt follow the LSP because it is expected to add to db.
+
+			//public override void Add(Database db)
+			//{
+			//	throw new Exception("Not allowed");
+			//}
+		}
+		public class GoldCustomer : Customer
+		{
+			public override int Discount(int sales)
+			{
+				return BaseDiscount - sales - 100;
+			}
+		}
+
+		public class SilverCustomer : Customer
+		{
+			public override int Discount(int sales)
+			{
+				return BaseDiscount - sales - 50;
+			}
+		}
+		class ViolatingLiskovs
+		{
+			public void ParseCustomers()
+			{
+				var database = new Database();
+				var customers = new List<Customer>
+				   {
+					   new GoldCustomer(),
+					   new SilverCustomer(),
+					   new Enquiry() // This is valid
+	               };
+
+
+				foreach (Customer c in customers)
+				{
+					// Enquiry.Add() will throw an exception here!
+					c.Add(database);
+				}
+			}
+		}
